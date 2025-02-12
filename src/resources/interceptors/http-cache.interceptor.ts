7276ab29dc5272a1f5d5b9ca7a9574912fa8df5a
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-12 19:56:47 -0300

    feat: :sparkles: adjust the cache to function exclusively with GET methods

--- src/resources/interceptors/http-cache.interceptor.ts
+++ src/resources/interceptors/http-cache.interceptor.ts
@@ -1,9 +1,16 @@
 import { CacheInterceptor } from "@nestjs/cache-manager";
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, HttpServer, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class HttpCacheInterceptor extends CacheInterceptor {
   trackBy(context: ExecutionContext): string | undefined {
+    const request = context.switchToHttp().getRequest()
+    const isGetRequest = request.method === 'GET';
+
+    if (!isGetRequest) {
+      return undefined;
+    }
+
     const { url } = context.getArgs()[0];
     const [_, endpoint, id] = url.split('/')
     return `${endpoint}-${id}`;
