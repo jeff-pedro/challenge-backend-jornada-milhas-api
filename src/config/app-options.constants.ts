6d2f7a5e3f88d1cede5ab6fac66f19674ca38be8
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-20 18:32:38 -0300

    feat: add multer configuration for dynamic photo upload paths

--- src/config/app-options.constants.ts
+++ src/config/app-options.constants.ts
@@ -1,6 +1,9 @@
 import KeyvRedis from "@keyv/redis";
 import { CacheModuleAsyncOptions } from "@nestjs/cache-manager";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MulterModuleAsyncOptions } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import * as fs from 'fs';
 
 const getRedisConfig = (configService: ConfigService) => ({
   socket: {
@@ -12,12 +15,30 @@ const getRedisConfig = (configService: ConfigService) => ({
 export const RedisOptions: CacheModuleAsyncOptions = {
       isGlobal: true,
       imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         stores: [
           new KeyvRedis(getRedisConfig(configService))
         ],
-        ttl: parseInt(configService.get<string>('CACHE_TTL')!), // tempo de vida dos dados
+        ttl: parseInt(configService.get<string>('CACHE_TTL')!),
       }),
+      inject: [ConfigService],
     }
-    
+  
+export const MulterOptions: MulterModuleAsyncOptions = {
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+              storage: diskStorage({
+                destination: (req, file, cb) => {
+                  const { id } = req.params;
+                  const uploadPath = `${configService.get<string>('UPLOAD_DESTINATION_PATH')}/${id}`;
+          
+                  if (!fs.existsSync(uploadPath)) {
+                    fs.mkdirSync(uploadPath, { recursive: true });
+                  }
+          
+                  cb(null, uploadPath);
+                },
+              })
+        }),
+        inject: [ConfigService],
+}
