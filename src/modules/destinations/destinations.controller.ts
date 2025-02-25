3c74b5abfd9e31f48bda07109ada6b1f78c84cae
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-24 21:20:55 -0300

    feat: :sparkles: enhance uploadPhotos method with file validation

--- src/modules/destinations/destinations.controller.ts
+++ src/modules/destinations/destinations.controller.ts
@@ -10,6 +10,8 @@ import {
   ParseUUIDPipe,
   UseInterceptors,
   UploadedFiles,
+  ParseFilePipeBuilder,
+  HttpStatus,
 } from '@nestjs/common';
 import { DestinationsService } from './destinations.service';
 import { CreateDestinationDto } from './dto/create-destination.dto';
@@ -18,6 +20,7 @@ import { ListDestinationDto } from './dto/list-destination.dto';
 import { Destination } from './entities/destination.entity';
 import { Public } from '../../resources/decorators/public-route.decorator';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { Photo } from '../photos/entities/photo.entity';
 
 @Controller('destinations')
 export class DestinationsController {
@@ -34,10 +37,16 @@ export class DestinationsController {
   @UseInterceptors(FilesInterceptor('files'))
   async uploadPhotos(
     @Param('id', ParseUUIDPipe) id: string,
-    @UploadedFiles() files: Express.Multer.File[]
-  ) {
-    await this.destinationsService.attachPhotos(id, files);
-    return { message: `Destination #${id} was updated` };
+    @UploadedFiles(
+      new ParseFilePipeBuilder()
+        .addMaxSizeValidator({ maxSize: 1024 * 1024 * 5 })
+        .addFileTypeValidator({ fileType: 'image/jpeg' })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    ) files: Express.Multer.File[]
+  ): Promise<Photo[]> {
+    return this.destinationsService.attachPhotos(id, files);
   }
 
   @Public()
