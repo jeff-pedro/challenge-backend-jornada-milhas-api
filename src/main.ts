8ff79ceeb23d4491778cc36625db6560c940d330
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-03-01 14:53:21 -0300

    feat: :sparkles: add integration to swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,28 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     abortOnError: false,
     cors: true,
   });
+
+  const { version, homepage, description, author } = require('../package.json');
+
+  const config = new DocumentBuilder()
+    .setTitle('Jornada Milhas API')
+    .setDescription(description)
+    .setVersion(version)
+    .setContact('Development contact', homepage, author.email)
+    .setExternalDoc('Github Wiki', 'https://github.com/jeff-pedro/challenge-backend-jornada-milhas/wiki')
+    .addBearerAuth()
+    .build();
+    
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
   const configService = app.get(ConfigService<{ app: { port: number } }, true>);
   const port = configService.get('app.port', { infer: true });
 
@@ -26,5 +42,6 @@ async function bootstrap() {
   });
 
   await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
