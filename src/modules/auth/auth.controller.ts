87914218cf49cca5fddef79c6496857c613336cc
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-03-04 23:20:34 -0300

    refactor: :recycle: enhance login endpoint documentation with response examples

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,13 +2,27 @@ import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { Public } from '../../resources/decorators/public-route.decorator';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /**
+   * 
+   * Login a user
+   * 
+   * @remarks This operation allows you to log in as a user and receive an access token.
+   * 
+   * @throws {400} Malformatted request body, invalid input.
+   * @throws {401} Unauthorized. User or password invalid. 
+   */
   @Public()
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ 
+    description: 'Successful operation.', 
+    example: { access_token: 'string' } 
+  })
   @Post('login')
   async login(@Body() { email, password }: SignInDto) {
     return this.authService.singIn(email, password);
