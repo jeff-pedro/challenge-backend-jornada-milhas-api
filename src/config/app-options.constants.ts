bf97c0480b8aacbab77e64f0f48d48e9787752fc
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-03-06 13:09:08 -0300

    feat: :sparkles: integrate AWS S3 for photo uploads and update multer storage configuration

--- src/config/app-options.constants.ts
+++ src/config/app-options.constants.ts
@@ -4,6 +4,8 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MulterModuleAsyncOptions } from "@nestjs/platform-express";
 import { diskStorage } from "multer";
 import * as fs from 'fs';
+import { S3Client } from "@aws-sdk/client-s3";
+import * as multerS3 from 'multer-s3';
 
 const getRedisConfig = (configService: ConfigService) => ({
   socket: {
@@ -11,6 +13,38 @@ const getRedisConfig = (configService: ConfigService) => ({
     port: parseInt(configService.get<string>('REDIS_PORT') ?? '6379')
   }, 
 })
+
+const getMulterS3StorageConfig = (configService: ConfigService) => (
+  // console.log('OI'),
+  multerS3({
+    s3: new S3Client({
+      region: configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: configService.get<string>('AWS_ACCESS_KEY_ID') || '',
+        secretAccessKey: configService.get<string>('AWS_SECRET_ACCESS_KEY') || ''
+      }
+    }),
+    bucket: configService.get<string>('AWS_S3_BUCKET') || '',
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString())
+    }
+  })
+);
+
+const getMulterDiskStorageConfig = (configService: ConfigService) => (
+  diskStorage({
+    destination: (req, file, cb) => {
+      const { id } = req.params;
+      const uploadPath = `${configService.get<string>('UPLOAD_DESTINATION_PATH')}/${id}`;
+
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
+
+      cb(null, uploadPath);
+    },
+  })
+);
 
 export const RedisOptions: CacheModuleAsyncOptions = {
       isGlobal: true,
@@ -23,22 +57,13 @@ export const RedisOptions: CacheModuleAsyncOptions = {
       }),
       inject: [ConfigService],
     }
-  
+
 export const MulterOptions: MulterModuleAsyncOptions = {
         imports: [ConfigModule],
         useFactory: (configService: ConfigService) => ({
-              storage: diskStorage({
-                destination: (req, file, cb) => {
-                  const { id } = req.params;
-                  const uploadPath = `${configService.get<string>('UPLOAD_DESTINATION_PATH')}/${id}`;
-          
-                  if (!fs.existsSync(uploadPath)) {
-                    fs.mkdirSync(uploadPath, { recursive: true });
-                  }
-          
-                  cb(null, uploadPath);
-                },
-              })
+          storage: configService.get<string>('AWS_S3_ENABLE') === "true"
+          ? getMulterS3StorageConfig(configService)
+          : getMulterDiskStorageConfig(configService)
         }),
         inject: [ConfigService],
 }
