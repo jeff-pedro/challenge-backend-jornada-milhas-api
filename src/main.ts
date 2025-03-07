d7649d5f81b27d5a64e91be5ebd05832b00b556e
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-03-07 15:06:07 -0300

    refactor: restructure bootstrap process and application setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { useContainer } from 'class-validator';
 import { SwaggerDocumentBuilder } from './swagger/swagger-document-builder';
 import { APP_DEFAULTS } from './config/constants/app.constants';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    abortOnError: false,
-    cors: true,
-  });
-
+function setupAplication(app: INestApplication) {
   app.setGlobalPrefix(APP_DEFAULTS.GLOBAL_PREFIX);
-
-  const swaggerDocumentBuilder = new SwaggerDocumentBuilder(app);
-  swaggerDocumentBuilder.setupSwagger();
-
-  const configService = app.get(ConfigService<{ app: { port: number } }, true>);
-  const port = configService.get('app.port', { infer: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -29,9 +18,23 @@ async function bootstrap() {
   );
 
   // Allows injecting dependencies into custom validator classes
-  useContainer(app.select(AppModule), {
-    fallbackOnErrors: true
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    abortOnError: false,
+    cors: true,
   });
+
+  setupAplication(app);
+    
+  const swaggerDocumentBuilder = new SwaggerDocumentBuilder(app);
+  swaggerDocumentBuilder.setupSwagger();
+  
+  const configService = app.get(ConfigService<{ app: { port: number } }, true>);
+  const port = configService.get('app.port', { infer: true });
+
 
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
