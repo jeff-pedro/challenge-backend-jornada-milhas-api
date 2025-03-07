a7a65737e4b1ee351f64989409ad17903a0edb7d
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-03-07 19:36:02 -0300

    feat: add cache configuration to enable and disable caching

--- src/resources/interceptors/http-cache.interceptor.ts
+++ src/resources/interceptors/http-cache.interceptor.ts
@@ -1,13 +1,25 @@
-import { CacheInterceptor } from "@nestjs/cache-manager";
-import { ExecutionContext, HttpServer, Injectable } from "@nestjs/common";
+import { CACHE_MANAGER, CacheInterceptor } from "@nestjs/cache-manager";
+import { ExecutionContext, Inject, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { Reflector } from "@nestjs/core";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class HttpCacheInterceptor extends CacheInterceptor {
+  constructor(
+    @Inject(CACHE_MANAGER) cacheManager: Cache,
+    reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {
+    super(cacheManager, reflector);
+  }
+
   trackBy(context: ExecutionContext): string | undefined {
     const request = context.switchToHttp().getRequest()
+    const isCacheEnabled = this.configService.get<boolean>('CACHE_ENABLED');
     const isGetRequest = request.method === 'GET';
 
-    if (!isGetRequest) {
+    if (!isGetRequest || !isCacheEnabled) {
       return undefined;
     }
 
